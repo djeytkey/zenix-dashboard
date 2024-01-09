40c4f95518a6f0bb3249a194976c73f46ec6559b
djeytkey <tarik.engineering@gmail.com>
2024-01-09 23:26:32 +0100

    first commit

---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div class="deznav">
+    <div class="deznav-scroll">
+        <div class="main-profile">
+            <img src="{{ asset('backend/images/Untitled-1.jpg') }}" alt="">
+            <a href="javascript:void(0);"><i class="fa fa-cog" aria-hidden="true"></i></a>
+            <h5 class="mb-0 fs-20 text-black "><span class="font-w400">Hello,</span> Marquez</h5>
+            <p class="mb-0 fs-14 font-w400">marquezzzz@mail.com</p>
+        </div>
+        <ul class="metismenu" id="menu">
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                    <i class="flaticon-144-layout"></i>
+                    <span class="nav-text">Dashboard</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="index.html">Dashboard</a></li>
+                </ul>
+
+            </li>
+            <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+                <i class="flaticon-077-menu-1"></i>
+                    <span class="nav-text">Reports</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="./app-profile.html">Order reports</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="copyright">
+            <p><strong>Zenix Crypto Admin Dashboard</strong> © 2021 All Rights Reserved</p>
+            <p class="fs-12">Made with <span class="heart"></span> by DexignZone</p>
+        </div>
+    </div>
+</div>
